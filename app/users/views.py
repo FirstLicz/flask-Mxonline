1fc21c32d4a981b51d2bf5f71fc08f9251f19e10
liczh <liczh@ecloudtech.com.cn>
2018-12-11 11:03:08 +0800

    我的收藏页面呈现

--- app/users/views.py
+++ app/users/views.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, current_user, login_required
 from flask import current_app
 
 from . import users
-from ..models import User, Course
+from ..models import User, Course, UserFavorite, Teacher, CourseOrg
 from ..utils.utils import Pagination
 
 
@@ -27,11 +27,62 @@ def user_courses():
     )
 
 
-@users.route('/collects/')
+@users.route('/collects/orgs/')
 @login_required
-def user_collects():
+def user_collects_orgs():  # 我收藏的机构
+    """
+        ('1', '机构'),
+    """
+    page = request.args.get('page', 1, type=int)
+    user_favorites = UserFavorite.query.filter_by(fav_type=1, user_id=current_user.id)
+    fav_list = []
+    if user_favorites:
+        for elem in user_favorites:
+            query_object = CourseOrg.query.get_or_404(elem)
+            fav_list.append(query_object)
+    pagination = Pagination(page=page, per_page=6, items=fav_list)
     return render_template(
-        'users/usercenter-mycourse.html',
+        'users/usercenter-fav-org.html',
+        pagination=pagination,
+    )
+
+
+@users.route('/collects/teachers/')
+@login_required
+def user_collects_teachers():  # 我收藏的讲师
+    """
+        ('2', '讲师'),
+    """
+    page = request.args.get('page', 1, type=int)
+    user_favorites = UserFavorite.query.filter_by(fav_type=2, user_id=current_user.id)
+    fav_list = []
+    if user_favorites:
+        for elem in user_favorites:
+            query_object = Teacher.query.get_or_404(elem)
+            fav_list.append(query_object)
+    pagination = Pagination(page=page, per_page=6, items=fav_list)
+    return render_template(
+        'users/usercenter-fav-teacher.html',
+        pagination=pagination,
+    )
+
+
+@users.route('/collects/courses/')
+@login_required
+def user_collects_courses():  # 我收藏的课程
+    """ ('0', '课程'),
+    """
+    page = request.args.get('page', 1, type=int)
+    user_favorites = UserFavorite.query.filter_by(fav_type=0, user_id=current_user.id)
+    fav_list = []
+    if user_favorites:
+        for elem in user_favorites:
+            query_object = Course.query.get_or_404(elem)
+            fav_list.append(query_object)
+    pagination = Pagination(page=page, per_page=6, items=fav_list)
+    return render_template(
+        'users/usercenter-fav-course.html',
+        pagination=pagination,
     )
 
 
